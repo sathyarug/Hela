8f9384301daae8a63a378ab58f1670a9f7f8a13c
ChamilaP <ChamilaP@KP-LP-ChamilaP.helaintimates.com>
2018-06-08 11:51:45 +0530

    Base validator model class. All models should extend this class.

---
+++ app/BaseValidator.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class BaseValidator extends Model
+{
+    protected $rules = array();
+    protected $errors;
+    protected $errors_str;
+
+    public function validate($data)
+    {
+        // make a new validator object
+        $v = \Illuminate\Support\Facades\Validator::make($data, $this->rules);
+
+        // check for failure
+        if ($v->fails()) {
+            // set errors and return false
+            $this->errors = $v->errors();
+            $this->$errors_str = implode(",",$v->messages()->all());
+            return false;
+        }
+        // validation pass
+        return true;
+    }
+
+    public function errors()
+    {
+        return $this->errors;
+    }
+    
+    public function errors_tostring(){
+        return $this->errors_str;
+    }
+}
