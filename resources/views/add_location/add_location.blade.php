42de9b70ca2ba055f5914373454893800386aad0
TharinduD <TharinduD@Kp-Lp-Tharindud.helaintimates.com>
2018-06-18 10:33:07 +0530

    location

--- resources/views/add_location/add_location.blade.php
+++ resources/views/add_location/add_location.blade.php
@@ -142,7 +142,7 @@
 <script type="text/javascript" src="assets/js/core/libraries/jquery_ui/interactions.min.js"></script>
 <script type="text/javascript" src="assets/js/plugins/forms/selects/select2.min.js"></script>
 <script type="text/javascript" src="assets/js/pages/form_select2.js"></script>
-<!-- /Select with search -->
+<!-- //Select with search -->
 
 <!-- picker_date -->
 <script type="text/javascript" src="assets/js/plugins/notifications/jgrowl.min.js"></script>
