7389a833cd2122fab091bba45d73d4336c553bbc
TharinduD <TharinduD@Kp-Lp-Tharindud.helaintimates.com>
2018-06-18 11:37:11 +0530

    location

--- resources/views/layout/main.blade.php
+++ resources/views/layout/main.blade.php
@@ -1,11 +1,10 @@
-
 <!DOCTYPE html>
 <html lang="en">
     <head>
-        <meta name="csrf-token" content="{{ csrf_token() }}">
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="csrf-token" content="{{ csrf_token() }}">
         <title>@yield('title')</title>
         <link rel="icon" href="assets\images\icon.jpg">
         <!-- Global stylesheets -->
