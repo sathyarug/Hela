d380a8632b092728f84e334d113d84e7062034a2
malingaj <malingaj@Pl-Lp-Malingaj.helaclothing.com>
2018-07-05 15:19:19 +0530

    stores module

---
+++ routes/web.php
@@ -0,0 +1,102 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+//login
+Route::get('/', 'Auth\LoginController@showLogin');
+
+Route::post('login', array('uses' => 'Auth\LoginController@doLogin'));
+
+Route::get('logout', 'Auth\LoginController@logout');
+
+Route::get('reset', 'Auth\ForgotPasswordController@reset');
+
+Route::get('select-location', 'Auth\LoginController@selectLocation');
+
+Route::POST('loginWithLoc', 'Auth\LoginController@loginWithLoc');
+
+
+Route::get('/recover', function () {
+    return view('recover');
+});
+
+Route::get('/home', function () {
+    return view('dashboard');
+});
+
+// Country module
+Route::get('create_country','CountryController@index');
+Route::get('get_all_country','CountryController@show');
+Route::delete('delete_country/{country_id}','CountryController@delete');
+Route::get('edit_country/{country_id}','CountryController@edit');
+Route::post('update_country/{country_id}','CountryController@update');
+Route::post('insertCountry','CountryController@insertCountry');
+
+//GRN module
+Route::get('grn_details','GrnController@grnDetails');
+
+
+Route::get('register', function () {
+    return view('user.register');
+});
+
+
+Route::post('register', array('uses'=>'UserController@store'));
+
+//currency routes
+Route::get('currency.new',['uses' => 'CurrencyController@new_currency']);
+
+Route::post('currency.save','CurrencyController@save');
+
+Route::get('currency.get_currency_list','CurrencyController@get_currency_list');
+
+Route::get('currency.get','CurrencyController@get_currency');
+
+//payment term routes
+Route::get('payment-term.new','PaymentTermController@new_payment_term');
+
+Route::post('payment-term.save','PaymentTermController@save');
+
+Route::get('payment-term.get_payment_term_list','PaymentTermController@get_payment_term_list');
+
+Route::get('payment-term.get','PaymentTermController@get_payment_term');
+
+
+// add location
+
+Route::get('add_location', function () { return view('add_location/add_location'); });
+
+Route::post('Mainsource.postdata','MainSourceController@postdata');
+
+Route::get('Mainsource.loaddata','MainSourceController@loaddata');
+
+Route::get('Mainsource.check_code','MainSourceController@check_code');
+
+Route::get('Mainsource.edit','MainSourceController@edit');
+
+Route::get('Mainsource.delete','MainSourceController@delete');
+
+Route::get('Mainsource.load_list','MainSourceController@select_Source_list');
+
+Route::get('Maincluster.loaddata','MainClusterController@loaddata');
+
+Route::get('Mainlocation.loaddata','MainLocationController@loaddata');
+
+// close add location
+Route::get('add_stores', function () { return view('add_stores/add_stores'); });
+Route::post('OrgStores.postdata','OrgStoresController@postdata');
+Route::get('OrgStores.loaddata','OrgStoresController@loaddata');
+Route::get('OrgStores.edit','OrgStoresController@edit');
+Route::get('OrgStores.delete','OrgStoresController@delete');
+Route::get('OrgStores.check_Store_Name','OrgStoresController@check_Store_Name');
+Route::get('OrgStores.load_fac_locations','OrgStoresController@load_fac_locations');
+Route::get('OrgStores.load_fac_section','OrgStoresController@load_fac_section');
