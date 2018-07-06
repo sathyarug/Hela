8c53811717045761fac483f814e631e14557c964
HELAINTIMATES\RohanJ <rohanj@helaclothing.com>
2018-07-06 17:33:25 +0530

    sql

--- routes/web.php
+++ routes/web.php
@@ -186,7 +186,7 @@ Route::get('Mainlocation.load_section_list','Org\Location\MainLocationController
 Route::post('Mainlocation.save_section','Org\Location\MainLocationController@save_section');
 Route::get('Mainlocation.section','Org\Location\MainLocationController@edit_load_section');
 
-
+//new
 Route::get('MainSubLocation.loaddata','Org\Location\MainSubLocationController@loaddata');
 Route::get('MainSubLocation.load_list','Org\Location\MainSubLocationController@load_list');
 Route::get('MainSubLocation.check_code','Org\Location\MainSubLocationController@check_code');
