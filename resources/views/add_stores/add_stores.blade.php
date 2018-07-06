e3e6cc34177a7965c48fb808817253d7aaa4e052
malingaj <malingaj@Pl-Lp-Malingaj.helaclothing.com>
2018-07-06 11:28:52 +0530

    stores module

--- resources/views/add_stores/add_stores.blade.php
+++ resources/views/add_stores/add_stores.blade.php
@@ -1,12 +1,12 @@
 
 @extends('layout.main')
 
-@section('title') Form Details @endsection
+@section('title') Stores Configuration @endsection
 @section('load_css')
 
 @endsection
 
-@section('m_add_location') class = 'active' @endsection
+@section('add_stores') class = 'active' @endsection
 
 @section('body')
 <!-- Page header -->
@@ -53,7 +53,7 @@
         <div class="col-md-12">
             <div class="panel panel-flat">
                 <div class="panel-heading">
-                    <h6 class="panel-title">Add Location</h6>
+                    <h6 class="panel-title">Stores Configuration</h6>
 
                     <div class="heading-elements">
                         <ul class="icons-list">
@@ -68,29 +68,28 @@
 
                     <div class="tabbable">
                         <ul class="nav nav-tabs nav-tabs-highlight nav-justified">
-                            <li class="active"><a href="#highlighted-justified-tab1" data-toggle="tab">Main Sourse</a></li>
-                            <li><a href="#highlighted-justified-tab2" data-toggle="tab">Main Cluster</a></li>
-                            <li><a href="#highlighted-justified-tab3" data-toggle="tab">Main Company</a></li>
-                            <li><a href="#highlighted-justified-tab4" data-toggle="tab">Location</a></li>
+                            <li class="active"><a href="#highlighted-justified-tab1" data-toggle="tab">Stores</a></li>
+                            <li><a href="#highlighted-justified-tab2" data-toggle="tab">Stores Location</a></li>
+                            <li><a href="#highlighted-justified-tab3" data-toggle="tab">Bin</a></li>
 
                         </ul>
 
                         <div class="tab-content">
-                            <div class="tab-pane active" id="highlighted-justified-tab1">
+                           <div class="tab-pane active" id="highlighted-justified-tab1">
 
-                                @include('add_location.main_source')
+                                @include('add_stores.main_stores')
 
                             </div>
 
                             <div class="tab-pane" id="highlighted-justified-tab2">
 
-                                @include('add_location.main_cluster')
+                                @include('add_stores.stores_location')
 
                             </div>
 
                             <div class="tab-pane" id="highlighted-justified-tab3">
 
-                                @include('add_location.main_location')
+                                @include('add_stores.stores_bins')
 
                             </div>
 
@@ -136,12 +135,12 @@
 <script type="text/javascript" src="assets/js/plugins/forms/styling/switchery.min.js"></script>
 <script type="text/javascript" src="assets/js/plugins/forms/styling/uniform.min.js"></script>
 
-<script type="text/javascript" src="js/add_location_js/add_location_js.js"></script>
+<script type="text/javascript" src="js/add_stores_js/add_stores_js.js"></script>
 <script type="text/javascript" src="js/application.js"></script>
 
 <script type="text/javascript" src="assets/js/core/libraries/jquery_ui/interactions.min.js"></script>
 <script type="text/javascript" src="assets/js/plugins/forms/selects/select2.min.js"></script>
-<script type="text/javascript" src="assets/js/pages/form_select2.js"></script>
+
 <!-- //Select with search -->
 
 <!-- picker_date -->
@@ -158,6 +157,9 @@
 
 <script type="text/javascript" src="assets/js/plugins/forms/validation/validate.min.js"></script>
 <script type="text/javascript" src="assets/js/plugins/notifications/sweet_alert.min.js"></script>
+<script type="text/javascript">
+    $('.select-search').select2();
+</script>
 
 
 @endsection
---
+++ resources/views/supplier/supplier.blade.php
@@ -0,0 +1,157 @@
+
+@extends('layout.main')
+@section('title') Supplier @endsection
+@section('supplier') class = 'active' @endsection
+@section('body')
+<!-- Page header -->
+<div class="page-header page-header-default ">
+
+
+    <div class="breadcrumb-line">
+        <ul class="breadcrumb">
+            <li><a href="index.html"><i class="icon-home2 position-left"></i> Home</a></li>
+            <li class="active">@yield('title')</li>
+        </ul>
+
+        <ul class="breadcrumb-elements">
+            <li><a href="#"><i class="icon-comment-discussion position-left"></i> Support</a></li>
+            <li class="dropdown">
+                <a href="#" class="dropdown-toggle" data-toggle="dropdown">
+                    <i class="icon-gear position-left"></i>
+                    Settings
+                    <span class="caret"></span>
+                </a>
+
+                <ul class="dropdown-menu dropdown-menu-right">
+                    <li><a href="#"><i class="icon-user-lock"></i> Account security</a></li>
+                    <li><a href="#"><i class="icon-statistics"></i> Analytics</a></li>
+                    <li><a href="#"><i class="icon-accessibility"></i> Accessibility</a></li>
+                    <li class="divider"></li>
+                    <li><a href="#"><i class="icon-gear"></i> All settings</a></li>
+                </ul>
+            </li>
+        </ul>
+    </div>
+</div>
+<!-- /page header -->
+
+
+<!-- Content area -->
+<div class="content">
+
+
+    <div class="col-md-12">
+
+        <!-- Basic layout-->
+
+        <div class="col-md-12">
+            <div class="panel panel-flat">
+                <div class="panel-heading">
+                    <h6 class="panel-title">Supplier</h6>
+
+                    <div class="heading-elements">
+                        <ul class="icons-list">
+                            <li><a data-action="collapse"></a></li>
+                            <li><a data-action="reload"></a></li>
+                            <li><a data-action="close"></a></li>
+                        </ul>
+                    </div>
+                </div>
+
+                <div class="panel-body">
+
+                    <div class="tabbable">
+
+                        <div class="tab-content">
+                            <div class="tab-pane active" id="highlighted-justified-tab1">
+
+                                <div class="col-md-12">
+
+                                    <div class="text-right">
+                                        <button type="button" class="btn bg-teal-400 btn-labeled btn-primary btn-xs" onclick="addEditSupplier(0)"><b><i class="icon-plus3"></i></b>Add New</button>
+                                        {{--<button type="button"  class="btn bg-teal-400 btn-labeled btn-primary btn-xs"--}}
+                                                {{--id="add_data"><b><i class="icon-plus3"></i></b>Add New</button>--}}
+                                    </div>
+
+
+                                    <table class="table datatable-basic" id="supplier_tbl">
+                                        <thead>
+                                        <tr>
+                                            <th class="text-center">Action</th>
+                                            <th>Supplier Name</th>
+                                            <th>Supplier Code</th>
+                                            <th>Supplier City</th>
+                                            <th>Supplier Phone</th>
+                                            <th>Supplier Email</th>
+                                            <th>Status</th>
+
+                                            <!-- <th class="text-center">Actions</th> -->
+                                        </tr>
+                                        </thead>
+                                        <tbody>
+
+                                        </tbody>
+
+
+                                    </table>
+
+
+                                </div>
+
+                                <!-- popup -->
+                                <div id="show_supplier" class="modal fade">
+                                    <div class="modal-dialog modal-lg">
+                                        <div class="modal-content">
+
+                                        </div>
+                                    </div>
+                                </div>
+
+                            </div>
+
+
+
+                            <div class="tab-pane" id="highlighted-justified-tab4">
+                                #
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <!-- /basic layout -->
+
+
+
+    </div>
+
+
+</div>
+<!-- /latest posts -->
+
+<!-- /mini modal -->
+
+
+@endsection
+
+
+
+
+@section('javascripy')
+
+    <script type="text/javascript" src="{{ URL::asset('assets/js/plugins/tables/datatables/datatables.min.js') }}"></script>
+    <script type="text/javascript" src="{{ URL::asset('assets/js/plugins/forms/selects/select2.min.js') }}"></script>
+
+
+    <script type="text/javascript" src="{{ URL::asset('js/supplier/supplier.js') }}"></script>
+    <script type="text/javascript" src="{{ URL::asset('js/application.js') }}"></script>
+    <script type="text/javascript" src="{{ URL::asset('assets/js/pages/datatables_basic.js') }}"></script>
+
+
+
+<script type="text/javascript" src="{{ URL::asset('assets/js/plugins/forms/validation/validate.min.js') }}"></script>
+<script type="text/javascript" src="{{ URL::asset('assets/js/plugins/notifications/sweet_alert.min.js') }}"></script>
+
+
+@endsection
