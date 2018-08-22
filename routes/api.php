9c756cde400b80a18528d4273e5d9db060711a34
HELACLOTHING\shanilad <shanilad@helaclothing.com>
2018-08-22 13:10:43 +0530

    Revert "Revert "anguler  first""
    This reverts commit b13a148d58838c4d7d9eafdc2add7f9cb220d10b.

--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,37 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::group([
+    'middleware' => 'api',
+    'prefix' => 'auth'
+], function ($router) {
+
+    Route::post('login', 'AuthController@login');
+    Route::post('logout', 'AuthController@logout');
+    Route::post('refresh', 'AuthController@refresh');
+    Route::post('me', 'AuthController@me');
+
+});
+
+//org routing.................................
+
+/*
+GET|HEAD  | api/countries  | countries.index    | App\Http\Controllers\Org\CountryController@index
+POST      | api/countries | countries.store    | App\Http\Controllers\Org\CountryController@store
+PUT|PATCH | api/countries/{country}  | countries.update   | App\Http\Controllers\Org\CountryController@update
+GET|HEAD  | api/countries/{country}  | countries.show     | App\Http\Controllers\Org\CountryController@show
+DELETE    | api/countries/{country}  | countries.destroy  | App\Http\Controllers\Org\CountryController@destroy */
+
+Route::apiResource('/countries','Org\CountryController');
+
+
+//Route::group(['middleware' => ['jwt.auth']], function() {
+  Route::GET('/sources','Test\SourceController@index');
+    Route::get('logout', 'AuthController@logout');
+    Route::get('test', function(){
+        return response()->json(['foo'=>'bar']);
+    });
+//});
+
+//Route::apiResource('/sources','Test\SourceController');
