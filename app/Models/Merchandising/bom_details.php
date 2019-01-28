f444464f7a2d8363fa073c33f27f07de09e4540f
nalinj <nalin@helaclothing.com>
2019-01-28 17:09:42 +0530

    Merge origin/master
    Conflicts:
    	app/Http/Controllers/Merchandising/BulkCosting/BulkCostingController.php
    	app/Models/Merchandising/CustomerOrder.php
    	routes/api.php

--- app/Models/Merchandising/bom_details.php
+++ app/Models/Merchandising/bom_details.php
@@ -29,21 +29,22 @@ class bom_details extends Model
 
     public $timestamps = false;
 
-    protected $fillable = ['bom_id','combine_id','item_code','item_color','item_size','unit_price','conpc','total_qty','total_value','supplier_id','artical_no','status','bal_qty','order_id','uom_id'];
+    protected $fillable = ['bom_id','combine_id','master_id','item_color','item_size','unit_price','conpc','total_qty','total_value','supplier_id','artical_no','status','bal_qty','order_id','uom_id'];
 
     public function GetBOMDetails($bomId){
 
         return DB::table('bom_details')
-                  ->join('item_master','item_master.master_id','bom_details.item_code')
+                  ->join('item_master','item_master.master_id','bom_details.master_id')
                   ->join('org_color','org_color.color_id','bom_details.item_color')
                   ->join('org_size','org_size.size_id','bom_details.item_size')
                   ->join('org_uom','org_uom.uom_id','bom_details.uom_id')
-                  ->join('bom_header','bom_header.bom_id','bom_details.bom_id') 
+                  ->join('bom_header','bom_header.bom_id','bom_details.bom_id')
+                  ->leftJoin('org_supplier','org_supplier.supplier_id','=','bom_details.supplier_id')
                   ->join('costing_bulk_details',function($join){
                     $join->on('costing_bulk_details.bulkheader_id','=','bom_header.costing_id')
-                         ->on('costing_bulk_details.item_id','=','bom_details.item_code');                         
+                         ->on('costing_bulk_details.item_id','=','bom_details.master_id');                         
                     })                                  
-                  ->select('item_master.master_id','item_master.master_description','bom_details.artical_no','org_color.color_name','org_size.size_name','org_uom.uom_description','org_uom.uom_id','bom_details.conpc','bom_details.item_wastage','bom_details.unit_price','bom_details.total_qty','bom_details.total_value','org_color.color_id','costing_bulk_details.moq','costing_bulk_details.mcq','org_size.size_id')
+                  ->select('item_master.master_id','item_master.master_description','bom_details.artical_no','org_color.color_name','org_size.size_name','org_uom.uom_description','org_uom.uom_id','bom_details.conpc','bom_details.item_wastage','bom_details.unit_price','bom_details.total_qty','bom_details.total_value','org_color.color_id','costing_bulk_details.moq','costing_bulk_details.mcq','org_size.size_id','org_supplier.supplier_name','org_supplier.supplier_id')
                   ->where('bom_details.bom_id',$bomId)->get();
 
 
