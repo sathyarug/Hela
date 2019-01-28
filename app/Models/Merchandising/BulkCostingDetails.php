7020d35a1f3af025a1948e94bb5f977eea76d79b
nalinj <nalin@helaclothing.com>
2019-01-28 17:09:54 +0530

    Merge origin/master
    Conflicts:
    	app/Http/Controllers/Merchandising/BulkCosting/BulkCostingController.php
    	app/Models/Merchandising/CustomerOrder.php
    	routes/api.php

--- app/Models/Merchandising/BulkCostingDetails.php
+++ app/Models/Merchandising/BulkCostingDetails.php
@@ -27,7 +27,7 @@ class BulkCostingDetails extends BaseValidator {
                     ->leftJoin('org_supplier','org_supplier.supplier_id','=','costing_bulk_details.supplier_id')
                     ->join('org_uom','org_uom.uom_id','costing_bulk_details.uom_id')
                     ->join('org_size','org_size.size_id','costing_bulk_details.size_id')
-                    ->select('item_master.master_id','item_master.master_description','costing_bulk_details.article_no','org_color.color_name','costing_bulk_details.net_consumption','costing_bulk_details.unit_price','costing_bulk_details.wastage','costing_bulk_details.gross_consumption','org_supplier.supplier_name','org_size.size_name','org_uom.uom_description','org_color.color_id','org_uom.uom_id','costing_bulk_details.moq','costing_bulk_details.mcq','costing_bulk_details.calculate_by_deliverywise','costing_bulk_details.order_type','costing_bulk_details.size_id')
+                    ->select('item_master.master_id','item_master.master_description','costing_bulk_details.article_no','org_color.color_name','costing_bulk_details.net_consumption','costing_bulk_details.unit_price','costing_bulk_details.wastage','costing_bulk_details.gross_consumption','org_supplier.supplier_name','org_size.size_name','org_uom.uom_description','org_color.color_id','org_uom.uom_id','costing_bulk_details.moq','costing_bulk_details.mcq','costing_bulk_details.calculate_by_deliverywise','costing_bulk_details.order_type','costing_bulk_details.size_id','org_supplier.supplier_id')
                     ->where('costing_bulk_details.bulkheader_id',$costingId)
                     ->get();        
        
