fbeea385e13801593dacc81a3bb87260a667e333
nalinj <nalin@helaclothing.com>
2019-02-06 16:22:20 +0530

    Merge origin/master
    Conflicts:
    	routes/api.php

--- app/Models/Merchandising/bom_details.php
+++ app/Models/Merchandising/bom_details.php
@@ -35,6 +35,8 @@ class bom_details extends Model
 
         return DB::table('bom_details')
                   ->join('item_master','item_master.master_id','bom_details.master_id')
+                  ->join('item_subcategory', 'item_subcategory.subcategory_id','item_master.subcategory_id')
+                  ->join('item_category','item_category.category_id','item_subcategory.category_id')
                   ->join('org_color','org_color.color_id','bom_details.item_color')
                   ->join('org_size','org_size.size_id','bom_details.item_size')
                   ->join('org_uom','org_uom.uom_id','bom_details.uom_id')
@@ -46,7 +48,7 @@ class bom_details extends Model
                     })                               
                   ->leftJoin('merc_color_options','merc_color_options.col_opt_id','=','costing_bulk_details.color_type_id')
                   ->leftJoin('merc_cut_direction','merc_cut_direction.cut_dir_id','=','costing_bulk_details.cut_dir_id')  
-                  ->select('item_master.master_id','item_master.master_description','bom_details.artical_no','org_color.color_name','org_size.size_name','org_uom.uom_description','org_uom.uom_id','bom_details.conpc','bom_details.item_wastage','bom_details.unit_price','bom_details.total_qty','bom_details.total_value','org_color.color_id','costing_bulk_details.moq','costing_bulk_details.mcq','org_size.size_id','org_supplier.supplier_name','org_supplier.supplier_id','merc_color_options.color_option','merc_cut_direction.cut_dir_description')
+                  ->select('item_master.master_id','item_master.master_description','bom_details.artical_no','org_color.color_name','org_size.size_name','org_uom.uom_description','org_uom.uom_id','bom_details.conpc','bom_details.item_wastage','bom_details.unit_price','bom_details.total_qty','bom_details.total_value','org_color.color_id','costing_bulk_details.moq','costing_bulk_details.mcq','org_size.size_id','org_supplier.supplier_name','org_supplier.supplier_id','merc_color_options.color_option','merc_cut_direction.cut_dir_description','item_category.category_code')
                   ->where('bom_details.bom_id',$bomId)->get();
 
 
