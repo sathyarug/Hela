e47b98af8a9b4018c589d2f6c52b2923df417712
HELACLOTHING\shanilad <shanilad@helaclothing.com>
2019-03-18 10:22:29 +0530

    fix bugs

--- app/Http/Controllers/Merchandising/BulkCosting/BulkCostingController.php
+++ app/Http/Controllers/Merchandising/BulkCosting/BulkCostingController.php
@@ -248,7 +248,7 @@ class BulkCostingController extends Controller {
         $dataArr = array();
         $styleData = \App\Models\Merchandising\styleCreation::find($style_id);
         $hader = \App\Models\Merchandising\BulkCosting::where('style_id', $style_id)->get()->toArray();
-        $country = \App\Models\Org\Country::find($styleData->customer->customer_country);
+        $country = \App\Models\Org\Country::find($styleData->customer->customer_county);
        
 
         $dataArr['style_remark'] = $styleData->remark;
