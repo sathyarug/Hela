52da5dfd147b90e86a005d788e330b3e636d2da6
Chathuranga_jayasuriya1993 <chathuranga.jayasuriya3@gmail.com>
2019-12-12 09:49:21 +0530

    Style list

--- routes/api.php
+++ routes/api.php
@@ -751,6 +751,8 @@ Route::prefix('reports/')->group(function(){
   Route::post('fabric_roll_barcode_print','Reports\FabticRollBarcode@getData');
   Route::post('update_print_status','Reports\FabticRollBarcode@updatePrint');
   Route::post('delete_barcode','Reports\FabticRollBarcode@deleteBarcode');
+  //Style list
+  Route::post('style-list','Reports\StyleListController@getStyles');
 });
 
 Route::prefix('common/')->group(function(){
